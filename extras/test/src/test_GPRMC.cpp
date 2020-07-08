2204de956c833881fce1859f09d68ce36ed9a47c
Alexander Entinger <cto@lxrobotics.com>
2020-07-08 08:10:06 +0200

    Extract encode 'code' into common function to facilitate code-reuse and eliminate copy'n'paste.

--- extras/test/src/test_GPRMC.cpp
+++ extras/test/src/test_GPRMC.cpp
@@ -15,6 +15,20 @@
 #include <Parser.h>
 
 /**************************************************************************************
+ * FUNCTION DEFINITION
+ **************************************************************************************/
+
+void encode(nmea::Parser & parser, std::string const & nmea)
+{
+  std::for_each(std::begin(nmea),
+                std::end(nmea),
+                [&parser](char const c)
+                {
+                  parser.encode(c);
+                });
+}
+
+/**************************************************************************************
  * TEST CODE
  **************************************************************************************/
 
@@ -36,12 +50,7 @@ SCENARIO("Valid GPRMC message received", "[GPRMC-02]")
   {
     std::string const GPRMC = "$GPRMC,062101.714,A,5001.869,N,01912.114,E,955535.7,116.2,290520,000.0,W*45\r\n";
 
-    std::for_each(std::begin(GPRMC),
-                  std::end(GPRMC),
-                  [&parser](char const c)
-                  {
-                    parser.encode(c);
-                  });
+    encode(parser, GPRMC);
 
     REQUIRE(parser.latitude()  == Approx(50.03114442));
     REQUIRE(parser.longitude() == Approx(19.20189679));
@@ -51,12 +60,7 @@ SCENARIO("Valid GPRMC message received", "[GPRMC-02]")
   {
     std::string const GPRMC = "$GPRMC,122311.239,A,4056.748,N,11212.614,W,,,290620,000.0,W*63\r\n";
 
-    std::for_each(std::begin(GPRMC),
-                  std::end(GPRMC),
-                  [&parser](char const c)
-                  {
-                    parser.encode(c);
-                  });
+    encode(parser, GPRMC);
 
     REQUIRE(parser.latitude()  == Approx(40.9458060446613));
     REQUIRE(parser.longitude() == Approx(-112.210235595703));
@@ -66,12 +70,7 @@ SCENARIO("Valid GPRMC message received", "[GPRMC-02]")
   {
     std::string const GPRMC = "$GPRMC,122311.239,A,2727.069,S,05859.190,W,,,290620,000.0,W*76\r\n";
 
-    std::for_each(std::begin(GPRMC),
-                  std::end(GPRMC),
-                  [&parser](char const c)
-                  {
-                    parser.encode(c);
-                  });
+    encode(parser, GPRMC);
 
     REQUIRE(parser.latitude()  == Approx(-27.4511422937699));
     REQUIRE(parser.longitude() == Approx(-58.986502289772));
@@ -81,12 +80,7 @@ SCENARIO("Valid GPRMC message received", "[GPRMC-02]")
   {
     std::string const GPRMC = "$GPRMC,122311.239,A,0610.522,S,10649.632,E,,,290620,000.0,W*6D\r\n";
 
-    std::for_each(std::begin(GPRMC),
-                  std::end(GPRMC),
-                  [&parser](char const c)
-                  {
-                    parser.encode(c);
-                  });
+    encode(parser, GPRMC);
 
     REQUIRE(parser.latitude()  == Approx(-6.17536097471491));
     REQUIRE(parser.longitude() == Approx(106.827192306519));
@@ -99,12 +93,7 @@ TEST_CASE("Valid GPRMC with speed over ground > 0 received", "[GPRMC-03]")
 
   std::string const GPRMC = ("$GPRMC,052856.105,A,5230.874,N,01321.056,E,085.7,206.4,080720,000.0,W*78\r\n");
 
-  std::for_each(std::begin(GPRMC),
-                std::end(GPRMC),
-                [&parser](char const c)
-                {
-                  parser.encode(c);
-                });
+  encode(parser, GPRMC);
 
   REQUIRE(parser.speed() == Approx(44.088f)); /* 85.7 kts ~= 44.088 m/s */
 }
