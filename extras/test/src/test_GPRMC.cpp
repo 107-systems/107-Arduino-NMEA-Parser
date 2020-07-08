5a608192c41735c37080894be6e97f4cd6a08180
Alexander Entinger <cto@lxrobotics.com>
2020-07-08 08:28:27 +0200

    Adding test code for testing multiple GPRMC messages in succession

--- extras/test/src/test_GPRMC.cpp
+++ extras/test/src/test_GPRMC.cpp
@@ -8,6 +8,7 @@
  **************************************************************************************/
 
 #include <string>
+#include <vector>
 #include <algorithm>
 
 #include <catch.hpp>
@@ -107,4 +108,61 @@ TEST_CASE("Valid GPRMC with track angle != 0 received", "[GPRMC-04]")
   encode(parser, GPRMC);
 
   REQUIRE(parser.course() == Approx(206.4f));
+}
+
+TEST_CASE("Multiple valid GPRMC messages received", "[GPRMC-05]")
+{
+  nmea::Parser parser;
+
+  std::vector<std::string> const GPRMC =
+  {
+    std::string("$GPRMC,052852.105,A,5230.868,N,01320.958,E,077.0,023.5,080720,000.0,W*79\r\n"),
+    std::string("$GPRMC,052853.105,A,5230.888,N,01320.967,E,084.5,069.2,080720,000.0,W*7A\r\n"),
+    std::string("$GPRMC,052854.105,A,5230.901,N,01321.000,E,085.8,099.8,080720,000.0,W*7D\r\n"),
+    std::string("$GPRMC,052855.105,A,5230.894,N,01321.038,E,084.3,138.7,080720,000.0,W*75\r\n"),
+  };
+
+  std::vector<float> const LATITUDE_EXPECTED =
+  {
+    52.514467f, 52.514800f, 52.515017f, 52.514900f
+  };
+
+  std::vector<float> const LONGITUDE_EXPECTED =
+  {
+    13.349300, 13.349450, 13.350000, 13.350633f
+  };
+
+  std::vector<float> const SPEED_EXPECTED =
+  {
+    39.6122f, 43.47056f, 44.139333f, 43.36767f
+  };
+
+  std::vector<float> const COURSE_EXPECTED =
+  {
+    23.5f, 69.2f, 99.8f, 138.7f
+  };
+
+  auto latitude  = LATITUDE_EXPECTED.begin();
+  auto longitude = LONGITUDE_EXPECTED.begin();
+  auto speed     = SPEED_EXPECTED.begin();
+  auto course    = COURSE_EXPECTED.begin();
+
+  std::for_each(std::begin(GPRMC),
+                std::end(GPRMC),
+                [&](std::string const & gprmc)
+                {
+                  encode(parser, gprmc);
+
+                  REQUIRE(parser.latitude () == Approx(*latitude));
+                  REQUIRE(parser.longitude() == Approx(*longitude));
+                  REQUIRE(parser.speed    () == Approx(*speed));
+                  REQUIRE(parser.course   () == Approx(*course));
+
+                  latitude  = std::next(latitude);
+                  longitude = std::next(longitude);
+                  speed     = std::next(speed);
+                  course    = std::next(course);
+                });
+
+  REQUIRE(parser.error() == nmea::Parser::Error::None);
 }
