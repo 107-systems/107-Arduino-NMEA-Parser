2fbabe5ea35ae6128143a22914bef8ba28c845a3
Alexander Entinger <cto@lxrobotics.com>
2020-10-12 09:52:33 +0200

    Adding GPRMC message of NEO-6M to checksum test code to ensure that checksum calculation still works for NEO-6M message

--- extras/test/src/test_Checksum.cpp
+++ extras/test/src/test_Checksum.cpp
@@ -18,16 +18,20 @@
  * TEST CODE
  **************************************************************************************/
 
-TEST_CASE("NMEA message with valid checksum", "[Checksum-01]")
+TEST_CASE("NMEA message with valid checksum - MTK3333", "[Checksum-01]")
 {
   std::string const GPRMC_CHECKSUM_OK = "$GPRMC,062101.714,A,5001.869,N,01912.114,E,955535.7,116.2,290520,000.0,W*45\r\n";
-
   REQUIRE(nmea::isChecksumOk(GPRMC_CHECKSUM_OK.c_str()) == true);
 }
 
-TEST_CASE("NMEA message with invalid checksum", "[Checksum-02]")
+TEST_CASE("NMEA message with valid checksum - ublox NEO-6M", "[Checksum-02]")
+{
+  std::string const GPRMC_CHECKSUM_OK = "$GPRMC,193517.00,A,4837.99895,N,01301.58584,E,0.793,,111020,,,A*7D\r\n";
+  REQUIRE(nmea::isChecksumOk(GPRMC_CHECKSUM_OK.c_str()) == true);
+}
+
+TEST_CASE("NMEA message with invalid checksum", "[Checksum-03]")
 {
   std::string const GPRMC_CHECKSUM_ERROR = "$GPRMC,062101.714,A,5001.869,N,01912.114,E,955535.7,116.2,290520,000.0,W*FF\r\n";
-
   REQUIRE(nmea::isChecksumOk(GPRMC_CHECKSUM_ERROR.c_str()) == false);
 }
