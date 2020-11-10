a5cfccfbf18e02888ad7501ed26ca3665e71253f
Alexander Entinger <cto@lxrobotics.com>
2020-11-10 06:15:06 +0100

    Provide constants INVALID_DATE/TIME/RMC to describe invalid datasets in one central location

--- src/ArduinoNmeaParser.cpp
+++ src/ArduinoNmeaParser.cpp
@@ -11,7 +11,6 @@
 
 #include "ArduinoNmeaParser.h"
 
-#include <math.h>
 #include <string.h>
 
 #include "nmea/GxRMC.h"
@@ -26,7 +25,7 @@ ArduinoNmeaParser::ArduinoNmeaParser(OnRmcUpdateFunc on_rmc_update)
 : _error{Error::None}
 , _parser_state{ParserState::Synching}
 , _parser_buf{{0}, 0}
-, _rmc{false, nmea::RmcSource::Unknown, NAN, NAN, NAN, NAN, NAN, {-1, -1, -1, -1}, {-1, -1, -1}}
+, _rmc{nmea::INVALID_RMC}
 , _on_rmc_update{on_rmc_update}
 {
 
