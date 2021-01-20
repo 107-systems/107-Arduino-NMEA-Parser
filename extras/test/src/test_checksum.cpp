ab8f592efd0f512b1b12af885b994875e39f3752
Alexander Entinger <cto@lxrobotics.com>
2021-01-20 14:43:42 +0100

    Remove no longer used function 'isChecksumToken'

--- extras/test/src/test_checksum.cpp
+++ extras/test/src/test_checksum.cpp
@@ -37,15 +37,3 @@ TEST_CASE("NMEA message with invalid checksum", "[checksum-03]")
   std::string const GPRMC_CHECKSUM_ERROR = "$GPRMC,062101.714,A,5001.869,N,01912.114,E,955535.7,116.2,290520,000.0,W*FF\r\n";
   REQUIRE(nmea::util::isChecksumOk(GPRMC_CHECKSUM_ERROR.c_str()) == false);
 }
-
-TEST_CASE("Test isChecksumToken(...) with valid checksum token", "[checksum-04]")
-{
-  std::string const VALID_CHECKSUM_TOKEN = "W*FF";
-  REQUIRE(nmea::util::isChecksumToken(VALID_CHECKSUM_TOKEN.c_str()) == true);
-}
-
-TEST_CASE("Test isChecksumToken(...) with invalid checksum token", "[checksum-04]")
-{
-  std::string const INVALID_CHECKSUM_TOKEN = "01912.114";
-  REQUIRE(nmea::util::isChecksumToken(INVALID_CHECKSUM_TOKEN.c_str()) == false);
-}
